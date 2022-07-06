e08888477390baa2f46b080bc60e8c4bc73eba6f
mazharenko <mazharenko.a@yandex.ru>
2022-07-06 10:51:42 +0500

    f# async implementation

--- src/FluentAssertions.Extensions.FSharp/EventualAssertions/EventualAssertions.fs
+++ src/FluentAssertions.Extensions.FSharp/EventualAssertions/EventualAssertions.fs
@@ -22,6 +22,31 @@ module EventualAssertions =
             f()
             repeat()
             
+    type EventualAssertionsBuilderAsync(timeout: TimeSpan, delay: TimeSpan) =
+        member __.Zero() = async.Zero()
+        member __.Delay f =
+            async {
+                use scope = new AssertionScope()
+                let stopwatch = Stopwatch.StartNew()            
+                let rec repeat() =
+                    async {
+                        if (not <| scope.HasFailures() || stopwatch.Elapsed > timeout)
+                        then ()                
+                        else
+                            do! Async.Sleep delay
+                            scope.Discard() |> ignore
+                            do! f()
+                            do! repeat()
+                    }
+                do! f()
+                do! repeat()
+            }
+        member __.Return f =
+            f
+            
         
     let eventually timeout delay = EventualAssertionsBuilder(timeout, delay) 
-    let eventuallyMs timeout delay = eventually (TimeSpan.FromMilliseconds timeout) (TimeSpan.FromMilliseconds delay) 
+    let eventuallyMs timeout delay = eventually (TimeSpan.FromMilliseconds timeout) (TimeSpan.FromMilliseconds delay)
+    
+    let eventuallyAsync timeout delay = EventualAssertionsBuilderAsync(timeout, delay) 
+    let eventuallyAsyncMs timeout delay = eventuallyAsync (TimeSpan.FromMilliseconds timeout) (TimeSpan.FromMilliseconds delay) 
