0bd6ef3ae9415ccd36adc8249e7b5240ea0735f0
mazharenko <mazharenko.a@yandex.ru>
2022-09-07 13:05:28 +0500

    cleanup

--- src/FluentAssertions.Extensions.FSharp/EventualAssertions/EventualAssertions.fs
+++ src/FluentAssertions.Extensions.FSharp/EventualAssertions/EventualAssertions.fs
@@ -3,51 +3,66 @@
 open System
 open System.Diagnostics
 open System.Threading
-open FluentAssertions.Execution;
+open FluentAssertions.Execution
 
 module EventualAssertions =
     type EventualAssertionsBuilder(timeout: TimeSpan, delay: TimeSpan) =
         member _.Zero() = ()
         member _.Delay f = f
+
         member _.Run f =
             use scope = new AssertionScope()
-            let stopwatch = Stopwatch.StartNew()            
-            let rec repeat() =
-                if (not <| scope.HasFailures() || stopwatch.Elapsed > timeout)
-                then ()                
+            let stopwatch = Stopwatch.StartNew()
+
+            let rec repeat () =
+                if (not <| scope.HasFailures()
+                    || stopwatch.Elapsed > timeout) then
+                    ()
                 else
                     Thread.Sleep delay
                     scope.Discard() |> ignore
-                    f()
-                    repeat()
-            f()
-            repeat()
-            
+                    f ()
+                    repeat ()
+
+            f ()
+            repeat ()
+
     type EventualAssertionsBuilderAsync(timeout: TimeSpan, delay: TimeSpan) =
         member _.Zero() = async.Zero()
         member _.Delay f = f
+
         member _.Run f =
             async {
                 use scope = new AssertionScope()
-                let stopwatch = Stopwatch.StartNew()            
-                let rec repeat() =
+                let stopwatch = Stopwatch.StartNew()
+
+                let rec repeat () =
                     async {
-                        if (not <| scope.HasFailures() || stopwatch.Elapsed > timeout)
-                        then ()                
+                        if (not <| scope.HasFailures()
+                            || stopwatch.Elapsed > timeout) then
+                            ()
                         else
                             do! Async.Sleep delay
                             scope.Discard() |> ignore
-                            do! f()
-                            do! repeat()
+                            do! f ()
+                            do! repeat ()
                     }
-                do! f()
-                do! repeat()
+
+                do! f ()
+                do! repeat ()
             }
+
         member _.Return f = f
-            
-        
-    let eventually timeout delay = EventualAssertionsBuilder(timeout, delay) 
-    let eventuallyMs timeout delay = eventually (TimeSpan.FromMilliseconds timeout) (TimeSpan.FromMilliseconds delay)
-    
-    let eventuallyAsync timeout delay = EventualAssertionsBuilderAsync(timeout, delay) 
-    let eventuallyAsyncMs timeout delay = eventuallyAsync (TimeSpan.FromMilliseconds timeout) (TimeSpan.FromMilliseconds delay) 
+
+
+    let eventually timeout delay =
+        EventualAssertionsBuilder(timeout, delay)
+
+    let eventuallyMs timeout delay =
+        eventually (TimeSpan.FromMilliseconds timeout) (TimeSpan.FromMilliseconds delay)
+
+    let eventuallyAsync timeout delay =
+        EventualAssertionsBuilderAsync(timeout, delay)
+
+    let eventuallyAsyncMs timeout delay =
+        eventuallyAsync (TimeSpan.FromMilliseconds timeout) (TimeSpan.FromMilliseconds delay)
